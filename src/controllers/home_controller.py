800c6888f33b8e538e44a077d70d61d3402d811d
Rafael <rafaelgermanio@gmail.com>
2024-06-26 10:44:51 -0300

    feat: Implementando metodo POST no home_controller

--- src/controllers/home_controller.py
+++ src/controllers/home_controller.py
@@ -1,16 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 home_controller = Blueprint("home_controller", __name__)
 
 
-@home_controller.route("/", methods=["GET"])
+@home_controller.route("/", methods=["GET", "POST"])
 def index():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = "O que deseja traduzir?"
+    translate_from = "pt"
+    translate_to = "en"
+    translated = "What do you want to translate?"
+
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+        translated = GoogleTranslator(
+            source="auto", target=translate_to).translate(text_to_translate)
     return render_template(
         "index.html",
-        languages=LanguageModel.list_dicts(),
-        text_to_translate="O que deseja traduzir?",
-        translate_from="pt",
-        translate_to="en",
-        translated="What do you want to translate?",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
         )
