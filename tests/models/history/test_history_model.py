0ad57428ada0f3d7163fc5e20c55c49e485949ad
Rafael <rafaelgermanio@gmail.com>
2024-06-26 18:47:49 -0300

    test: implementando test unitario para metodo list_as_json de HistoeyModel

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,12 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history = json.loads(HistoryModel.list_as_json())
+
+    assert len(history) == 2
+    assert history[0]["text_to_translate"] == "Hello, I like videogame"
+    assert history[1]["translate_from"] == "en"
+    assert history[1]["translate_to"] == "pt"
